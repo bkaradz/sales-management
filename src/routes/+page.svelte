268fea4c9fc37aa7455aab190dad167559e1aeb2
karadz <karadz@gmail.com>
2023-07-03 14:58:19 +0200

    added packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="d-flex flex-column min-vh-100">
+	<header class="bg-primary text-white py-5">
+		<h1 class="text-center">DaisyUI Admin Dashboard</h1>
+	</header>
+	<main class="py-5">
+		<section class="container">
+			<h2>This is the main section of the dashboard</h2>
+			<p>You can add any content here, such as tables, charts, graphs, etc.</p>
+		</section>
+	</main>
+	<footer class="bg-secondary text-white py-5">
+		<p class="text-center">Copyright &copy; 2023</p>
+	</footer>
+</div>
+
+<style lang="postcss">
+	@media (max-width: 768px) {
+		footer {
+			display: none;
+		}
+	}
+</style>
