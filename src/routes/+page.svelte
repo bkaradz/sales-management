f72c9ed8d4dc4b65969155310ea3cc7fd2ab78de
unknown <karadz@gmail.com>
2023-07-07 17:19:31 +0200

    WIP sign-up, sign-in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,6 @@
-<!-- <div class="d-flex flex-column min-vh-100">
-	<header class="bg-primary text-white py-5">
-		<h1 class="text-center">DaisyUI Admin Dashboard</h1>
-	</header>
-	<main class="py-5">
-		<section class="container">
-			<h2>This is the main section of the dashboard</h2>
-			<p>You can add any content here, such as tables, charts, graphs, etc.</p>
-		</section>
-	</main>
-	<footer class="bg-secondary text-white py-5">
-		<p class="text-center">Copyright &copy; 2023</p>
-	</footer>
-</div> -->
 
-<div class="grid-container">
+
+<!-- <div class="grid-container">
 	<div class="menu-icon">
 		<strong> &#9776;</strong>
 	</div>
@@ -59,11 +46,8 @@
 			<div class="card">Card</div>
 		</div>
 	</main>
-	<!-- <footer class="footer">
-		<div class="footer_copyright">&copy;2020</div>
-		<div class="footer_byline">Made with &hearts;</div>
-	</footer> -->
-</div>
+	
+</div> -->
 
 <style lang="postcss">
 	/* @media (max-width: 768px) {
