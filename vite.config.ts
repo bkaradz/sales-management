6f187dd4d0dd541706a7409c021bf8846e7a3127
karadz <karadz@gmail.com>
2023-11-16 17:52:38 +0200

    WIP payments

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { imagetools } from 'vite-imagetools';
+// import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()],
+	// plugins: [sveltekit(), imagetools()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
