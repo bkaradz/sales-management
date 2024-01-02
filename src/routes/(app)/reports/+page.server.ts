e964262873367ff041f3567a7878e5727a3e8d9a
karadz <karadz@gmail.com>
2024-01-02 19:18:26 +0200

    Puppeteer removed

--- src/routes/(app)/reports/+page.server.ts
+++ src/routes/(app)/reports/+page.server.ts
@@ -1,96 +1,9 @@
-import { redirect, type Actions } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import puppeteer from 'puppeteer';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-const input = {
-    username: process.env.printerUser,
-    password: process.env.printerPassword
-} as { username: string, password: string };
 
 export const load = (async () => {
     return {};
 }) satisfies PageServerLoad;
 
-export const actions: Actions = {
-    printPdf: async (event) => {
+export const actions: Actions = {}
 
-        const session = await event.locals.auth.validate()
-
-        if (!session) {
-            redirect(303, "/auth/login");
-        }
-
-        const data = await event.request.formData();
-        const formData = Object.fromEntries(data)
-
-        const origin = formData.origin as string
-        const pathname = formData.pathname as string
-        const urlArray = []
-        urlArray.push(origin)
-        urlArray.push(pathname)
-        const url = urlArray.join('')
-        
-        try {
-
-            const browser = await puppeteer.launch({
-                headless: "new"
-            });
-
-            // Create a new page
-            const page1 = await browser.newPage();
-
-            await page1.goto(`${origin}`, {
-                waitUntil: ['domcontentloaded', 'networkidle0']
-            });
-
-            // Login
-            await page1.type('#username', input.username);
-            await page1.type('#password', input.password);
-            await page1.click('#submit');
-            await page1.waitForNavigation();
-
-            // Get cookies
-            const cookies = await page1.cookies();
-
-            const page = await browser.newPage();
-            await page.setCookie(...cookies);
-
-            //Get HTML content from HTML file
-            await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
-
-            // To reflect CSS used for screens instead of print
-            await page.emulateMediaType('print');
-
-            // Download the PDF
-            const pdfBuffer = await page.pdf({
-                format: 'A4',
-                landscape: true,
-                printBackground: true
-            });
-
-            await browser.close();
-
-            const pdf = {
-                pdf: pdfBuffer.toString('base64')
-            };
-
-            return {
-                success: true,
-                data: JSON.stringify(pdf),
-            };
-        } catch (err) {
-            console.error("🚀 ~ file: +page.server.ts:60 ~ printPdf: ~ err:", err)
-            return {
-                errors: true,
-                message: "Failed to generate PDF"
-            }
-        }
-    }
-}
-
-const printPDF = async (url: string) => {
-
-};
--- src/routes/(app)/reports/+page.svelte
+++ src/routes/(app)/reports/+page.svelte
@@ -1,37 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';	
 	import { enhance } from '$app/forms';
-	import { toasts } from '$lib/stores/toasts.store';
-	import { Buffer } from 'buffer';
 	import { longPress } from '$lib/utility/inputSelectDirective.js';
-
-	export let form;
-
-	$: if (form?.success) {
-		const json = JSON.parse(form.data);
-
-		const pdfBuffer = Buffer.from(json.pdf, 'base64');
-
-		const file = new Blob([pdfBuffer], { type: 'application/pdf' });
-
-		const fileURL = URL.createObjectURL(file);
-
-		let fileLink = document.createElement('a');
-		fileLink.href = fileURL;
-		fileLink.setAttribute('target','_blank') 
-		// fileLink.download = `${'production'}.pdf`;
-		// fileLink.setAttribute('name', 'test.pdf')
-		fileLink.click();
-	}
-
-	$: if (form?.errors) {
-		toasts.add({
-			message: `${form.message}`,
-			type: 'error'
-		});
-	}
-
-	let pressed = false;
 
 	let counter = 0
 
@@ -48,11 +18,7 @@
 </svelte:head>
 <h1>Reports</h1>
 
-<form id="deleteForm" action="?/printPdf" method="post" use:enhance>
-	<input type="hidden" name="pathname" value="/reports/production" />
-	<input type="hidden" name="origin" value={$page.url.origin} />
-	<button class="btn btn-success">Print</button>
-</form>
+
 
 <button class="btn btn-primary mb-4" on:longPress={() => add()} use:longPress>
 	long press me add
